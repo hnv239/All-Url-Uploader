8c2ca9ae1858536da665f86666453d979235b91a
hnv239 <118403051+hnv239@users.noreply.github.com>
2022-11-18 01:48:51 +0700

    Fix invalid syntax

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -292,12 +292,12 @@ async def echo(bot, update):
 
         await chk.delete()
 
-        message = await bot.send_message(
+        await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.FORMAT_SELECTION.format(
                 Thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
             # reply_markup=reply_markup,
-            data=(cb_string_video).encode("UTF-8")
+            data=cb_string_video).encode("UTF-8"),
 
             reply_to_message_id=update.id
         )
