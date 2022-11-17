e2d580e66c47b6dc79b0dd5b5e1b0374a0dc6064
hnv239 <118403051+hnv239@users.noreply.github.com>
2022-11-18 02:54:41 +0700

    Revert

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -40,7 +40,6 @@ from Uploader.script import Translation
 from Uploader.functions.ran_text import random_char
 from Uploader.functions.display_progress import humanbytes
 from Uploader.functions.display_progress import humanbytes
-from Uploader.button import youtube_dl_call_back
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -296,12 +295,9 @@ async def echo(bot, update):
             chat_id=update.chat.id,
             text=Translation.FORMAT_SELECTION.format(
                 Thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
-            #reply_markup=reply_markup,
+            reply_markup=reply_markup,
             reply_to_message_id=update.id
         )
-        bot.register_next_step_handler(message, youtube_dl_call_back)
-        #message.data=cb_string_video
-        # await youtube_dl_call_back(bot, message)
     else:
         # fallback for nonnumeric port a.k.a seedbox.io
         inline_keyboard = []
